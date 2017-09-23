1489b6b3acc24f040e56a243f1341727ad589acc
Zach Johnson <bballzach64@gmail.com>
2017-09-23 18:31:48 +0000

    Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.acton_mailer.default_url_options = { host: 'nomster-zach-johnson.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
