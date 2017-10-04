b806bbaf00d0e073f510140c659da3b9c6727eb9
Zach Johnson <bbalzach64@gmail.com>
2017-10-04 14:33:12 +0000

    Trying to fix Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
 	def show
+		@user = User.find(params[:id])
 	end
-	
+
+	# # private
+
+	# # def users_params
+	# 	params.require(:user).permit(:)
+	# end 
 end
